4f936a5bf6248d3c82fd49f6935baf9cc37e0dbd
MariannaHon <imarianna96i@gmail.com>
2024-08-28 23:04:20 +0200

    commit-2
    made structure of app

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,11 @@
+
+
+const AuthForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AuthForm
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
+import { store, persistor } from './redux/store.js'
+import App from './components/App/App.jsx'
+import 'modern-normalize';
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 )
