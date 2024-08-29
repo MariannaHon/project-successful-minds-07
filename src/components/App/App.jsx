11d5c0e2b0bee075a4183609de131c6d64019082
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-08-29 22:42:55 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,10 @@
 import './App.css';
-//import { WaterСonsumptionTracker } from '../../components/WaterСonsumptionTracker/WaterСonsumptionTracker.jsx';
 
-// function App() {
-//   return (
-//     <>
-
-//     </>
-//   );
-// }
-
-// export default App;
-//import { useEffect, lazy } from 'react';
-//import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-// import { Layout } from './Layout';
-// import { PrivateRoute } from './PrivateRoute';
-// import { RestrictedRoute } from './RestrictedRoute';
-// import { refreshUser } from '../redux/auth/operations';
-// import { selectIsRefreshing } from '../redux/auth/selectors';
 
-// import { Toaster } from 'react-hot-toast';
 import WelcomePage from '../../pages/WelcomePage/WelcomePage.jsx';
-// const HomePage = lazy(() => import('../pages/homePage/HomePage'));
-// const RegistrationPage = lazy(() =>
-//   import('../pages/registrationPage/RegistrationPage')
-// );
-// const LoginPage = lazy(() => import('../pages/loginPage/LoginPage'));
-// const ContactsPage = lazy(() => import('../pages/contactsPage/ContactsPage'));
 
 export const App = () => {
-  //   const dispatch = useDispatch();
-  // const isRefreshing = useSelector(selectIsRefreshing);
-  // useEffect(() => {
-  //   dispatch(refreshUser());
-  // }, [dispatch]);
-
   return (
     <Routes>
       <Route path="/welcome" element={<WelcomePage />} />
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,14 +1,14 @@
 import { WaterСonsumptionTracker } from '../../components/WaterСonsumptionTracker/WaterСonsumptionTracker.jsx';
 
 import { WhyDrinkWater } from '../../components/WhyDrinkWater/WhyDrinkWater.jsx';
-
+import css from './WelcomePage.module.css';
 const WelcomePage = () => {
   return (
-    <>
+    <div className={css.welcomePage}>
       <WaterСonsumptionTracker />
 
       <WhyDrinkWater />
-    </>
+    </div>
   );
 };
 
