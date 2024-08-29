be0e30aef3e63dcf42be98bc4b3b4e4bda988b89
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-08-29 20:45:16 +0300

    Update WaterСonsumptionTracker.jsx

--- src/components/WaterСonsumptionTracker/WaterСonsumptionTracker.jsx
+++ src/components/WaterСonsumptionTracker/WaterСonsumptionTracker.jsx
@@ -2,13 +2,18 @@
 
 //import { useSelector, useDispatch } from 'react-redux';
 
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './WaterConsumptionTracker.module.css';
 
 const WaterСonsumptionTracker = () => {
+  const navigate = useNavigate();
+
+  const handleTryTrackerClick = () => {
+    navigate('/signup'); // Використовуємо navigate для переходу
+  };
   return (
-    <div className={styles.waterСonsumptionTracker}>
+    <div className={styles.waterСonsumptionTrackerBlockOne}>
       <h1 className={styles.waterСonsumptionTrackerHeader}>
         Water consumption tracker
       </h1>
@@ -17,31 +22,63 @@ const WaterСonsumptionTracker = () => {
         Record daily water intake and track your progress
       </h2>
 
-      <div className={styles.waterСonsumptionTrackerBlockOne}>
-        <h3 className={styles.waterСonsumptionTrackerBlockOneTitle}>
-          Tracker Benefits
-        </h3>
+      <h3 className={styles.waterСonsumptionTrackerBlockOneTitle}>
+        Tracker Benefits
+      </h3>
 
-        <ul className={styles.waterСonsumptionTrackerContainer}>
-          <li className={styles.waterСonsumptionTrackerItem}>
-            <span>&#128204;</span>
+      <ul className={styles.waterСonsumptionTrackerContainer}>
+        <li className={styles.waterСonsumptionTrackerItem}>
+          <span>
+            <svg
+              className={styles.iconCalendarDays}
+              width="40"
+              height="40"
+              aria-label="icon-calendar-days"
+            >
+              <use href="../../../public/symbol-defs.svg#icon-calendar-days"></use>
+            </svg>
+          </span>
 
-            <span>Habit drive</span>
-          </li>
+          <span>Habit drive</span>
+        </li>
 
-          <li className={styles.waterСonsumptionTrackerItem}>
-            <span>&#128200;</span>
+        <li className={styles.waterСonsumptionTrackerItem}>
+          <span>
+            <svg
+              className={styles.iconPresentationChartBar}
+              width="40"
+              height="40"
+              aria-label="icon-presentation-chart-bar"
+            >
+              <use href="../../../public/symbol-defs.svg#icon-presentation-chart-bar"></use>
+            </svg>
+          </span>
 
-            <span>View statistics</span>
-          </li>
+          <span>View statistics</span>
+        </li>
 
-          <li className={styles.waterСonsumptionTrackerItem}>
-            <span>&#128100;</span>
+        <li className={styles.waterСonsumptionTrackerItem}>
+          <span>
+            <svg
+              className={styles.iconWrenchScrewdriver}
+              width="40"
+              height="40"
+              aria-label="icon-wrench-screwdriver"
+            >
+              <use href="../../../public/symbol-defs.svg#icon-wrench-screwdriver"></use>
+            </svg>
+          </span>
 
-            <span>Personal rate setting</span>
-          </li>
-        </ul>
-      </div>
+          <span>Personal rate setting</span>
+        </li>
+      </ul>
+      <button
+        type="button"
+        className={styles.button}
+        onClick={handleTryTrackerClick}
+      >
+        Try tracker
+      </button>
     </div>
   );
 };
