923994495cbb600d2e6e7854d16f1b2640eae545
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-08-31 07:58:29 +0300

    add

--- src/components/WhyDrinkWater/WhyDrinkWater.jsx
+++ src/components/WhyDrinkWater/WhyDrinkWater.jsx
@@ -3,51 +3,47 @@ import { VscCircleFilled } from 'react-icons/vsc';
 const WhyDrinkWater = () => {
   return (
     <div className={css.whyDrinkWaterBlockTwo}>
-      <h3 className={css.WaterHeader}>Why drink water</h3>
-
       <ul className={css.whyDrinkWaterContainer}>
+        <li className={css.waterItem}>
+          <h3 className={css.WaterHeader}>Why drink water</h3>
+        </li>
+
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
           </span>
           Supply of nutrients to all organs
         </li>
-
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
           </span>
           Providing oxygen to the lungs
         </li>
-
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
           </span>
           Maintaining the work of the heart
         </li>
-
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
           </span>
           Release of processed substances
         </li>
-
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
           </span>
           Ensuring the stability of the internal environment
         </li>
-
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
           </span>
           <span>Maintaining within the normal temperature</span>
         </li>
-
         <li className={css.waterItem}>
           <span className={css.myIconSpan}>
             <VscCircleFilled size="14" />
