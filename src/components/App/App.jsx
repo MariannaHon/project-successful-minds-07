079c6bb58a476aaf57302f741205d5c49ac418e3
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-08-31 16:06:16 +0300

    add App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,43 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import SignInPage from '../../pages/SigninPage/SigninPage';
-import NotFound from '../../pages/NotFound/NotFound'
-function App() {
-  console.log('-------------APP');
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { RestrictedRoute } from '../RestrictedRoute/RestrictedRoute';
+import { SharedLayout } from '../SharedLayout/SharedLayout';
+import { PrivateRoute } from '../PrivateRoute/PrivateRoute';
 
-  return (
-    <>
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const SignupPage = lazy(() => import('../../pages/SignupPage/SignupPage'));
+const SigninPage = lazy(() => import('../../pages/SigninPage/SigninPage'));
+const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage'));
+const NotFoundPage = lazy(() =>
+  import('../../pages/NotFoundPage/NotFoundPage')
+);
+
+export default function App() {
+  return (<div>
+    <SharedLayout>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/signin" element={<SignInPage />} />
-         {/* <Route path="/signup" element={<SignUpPage />} />  */}
-         <Route path="*" element={<NotFound />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route
+          path="/signup"
+          element={
+            <RestrictedRoute redirectTo="/home" component={<SignupPage />} />
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <RestrictedRoute component={<SigninPage />} redirectTo="/home" />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute component={<HomePage />} redirectTo="/signin" />
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </>
-  );
+    </SharedLayout>
+  </div>
+);
 }
-
-export default App;
