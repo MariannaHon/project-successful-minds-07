efc56ec171b3feac559308a5fb67625df76e3654
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2024-09-02 02:11:30 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import { NavLink, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import UserAuth from '../UserAuth/UserAuth.jsx';
+import Logo from '../Logo/Logo.jsx';
 
 const Header = () => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
     return (
         <header>
-          {isLoggedIn ? <Navigate to="/home" replace /> : <NavLink to="/signup">Sign up</NavLink>}
+            <Logo />
+            <UserAuth />
         </header>
       );
 };
