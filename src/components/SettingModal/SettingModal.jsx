aa1718fb740b7e6767157f5e860ad094986c1768
Yuliia Diachenko <zirkanew82@gmail.com>
2024-09-02 11:10:10 +0300

    add media styles

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -39,7 +39,7 @@ const FeedbackSchema = Yup.object().shape({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 1008,
+    maxWidth: 1008,
     margin: 0,
     bgcolor: 'background.paper',
     border: 'none',
@@ -77,14 +77,12 @@ const FeedbackSchema = Yup.object().shape({
        <div>
         <button className={css.buttonSetting} onClick={handleOpen} ><IoSettingsOutline/>  Setting</button>
         <Modal open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
-        <Box sx={style} className={css.container}>
+        onClose={handleClose}  className={css.modalWindow}>
+        <Box sx={style} >
         <button onClick={handleClose} className={css.iconClose}><IoClose /></button>
 
         <div className={css.container}>
-<Formik initialValues={{ picked: "", name: "", email: "", outPassword:"", nPassword:""}} onSubmit={handleSubmit} 
+<Formik initialValues={{ gender: "", name: "", email: "", outPassword:"", nPassword:""}} onSubmit={handleSubmit} 
 validationSchema={FeedbackSchema} > 
     <Form>
       <div className={css.leftRightGroup}>
@@ -97,15 +95,16 @@ validationSchema={FeedbackSchema} >
             <a href="" className={css.link}><LuUpload /> Upload a photo</a>    
           </div>
           </div>   
-<FormControl className={css.radio}>
+<FormControl className={css.radio} >
       <FormLabel id="demo-row-radio-buttons-group-label"><h3>Your gender identity</h3></FormLabel>
       <RadioGroup
         row
         aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
+        defaultValue="female"
+        name="row-radio-buttons-group"        
       >
-        <FormControlLabel value="women" control={<Radio />} label="Woman" className={css.label} />
-        <FormControlLabel value="man" control={<Radio />} label="Man"  className={css.label}/>
+        <FormControlLabel value="female" control={<Radio />} label="Woman" className={css.label} />
+        <FormControlLabel value="male" control={<Radio />} label="Man"  className={css.label}/>
      
       </RadioGroup>
     </FormControl>
