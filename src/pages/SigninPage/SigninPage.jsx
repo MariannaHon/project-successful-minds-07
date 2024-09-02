a11952b61d0747509ea4985c2422df2654c23128
MariannaHon <imarianna96i@gmail.com>
2024-09-02 16:53:53 +0200

    ---

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -6,29 +6,29 @@ import css from './SigninPage.module.css';
 
 
 export default function Signin() {
-  console.log('--------------------------SIGNIN');
+    console.log('--------------------------SIGNIN');
 
-  return (
-    <div className={css.container}>
-      {/* <Helmet>
+    return (
+        <div className={css.container}>
+            {/* <Helmet>
         <title>SignIn to Tracker of Water</title>
       </Helmet> */}
 
-      {/* <Header /> */}
-      <div className={css.container_signin}>
-      <div className={css.background}>
-        <h2 className={css.title}>Sign In</h2>
-        
-        <AuthForm />
-        {/* <NavLink className={css.link} to="/forgot-password"> */}
-        {/* Forgot your password?
+            {/* <Header /> */}
+            <div className={css.container_signin}>
+                <div className={css.background}>
+                    <h2 className={css.title}>Sign In</h2>
+
+                    <AuthForm />
+                    {/* <NavLink className={css.link} to="/forgot-password"> */}
+                    {/* Forgot your password?
       </NavLink>  */}
-        <NavLink className={css.link} to="/signup">
-          Sign up
-        </NavLink>
+                    <NavLink className={css.link} to="/signup">
+                        Sign up
+                    </NavLink>
+                </div>
+            </div>
+            <div className={css.image}></div>
         </div>
-      </div>
-      <div className={css.image}></div>
-    </div>
-  );
+    );
 }
