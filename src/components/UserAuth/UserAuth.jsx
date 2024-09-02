7104f2ccef40ad7da01fdaf2173488befc8ef330
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2024-09-02 09:12:27 +0200

    Update UserAuth.jsx

---
+++ src/components/UserAuth/UserAuth.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
+const UserAuth = () => {
+
+    const navigate = useNavigate();
+
+    const authClick = () => {
+        navigate('/signin');
+    }
+
+    return (
+        <div>
+            <button type="button" onClick={authClick}>Sign in</button>
+        </div>
+    )
+}
+
+export default UserAuth
