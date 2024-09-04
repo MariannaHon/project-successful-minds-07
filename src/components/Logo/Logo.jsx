7bca16e88b11a92d5b9c28ce871a51e827e31b10
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-09-04 20:50:06 +0300

    header-fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,28 +1,32 @@
-
 import { useNavigate } from 'react-router-dom';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
-
 import { useSelector } from 'react-redux';
 import css from "./Logo.module.css";
 
 const Logo = () => {
-
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const navigate = useNavigate();
 
   const logoClick = () => {
     navigate(isLoggedIn ? "/home" : "/welcome");
-  }
+  };
 
   return (
-
-    <div onClick={logoClick}>
-      <svg width="40" height="48" className={css.icons}>
-        <use href="./dist/symbol-defs1.svg#icon-Logo-2" >
-        </use>
-      </svg>
+    <div className={css.logoWrapper} onClick={logoClick}>
+      <img 
+        src="/android-chrome-192x192.png" 
+        alt="Logo" 
+        width="40" 
+        height="48" 
+        className={css.icons} 
+      />
+      <p className={css.logoTekst}>
+        TRACKER
+        <br />
+        OF WATER
+      </p>
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,11 +1,7 @@
-
-
-import { useNavigate } from "react-router-dom"
-
+import { useNavigate } from "react-router-dom";
 import css from "./UserAuth.module.css";
 
 const UserAuth = () => {
-
   const navigate = useNavigate();
 
   const authClick = () => {
@@ -13,16 +9,15 @@ const UserAuth = () => {
   }
 
   return (
-
-    <div>
-      <button className={css.userAuthLink} type="button" onClick={authClick}>Sign in</button>
+    <div className={css.userAuthWrapper}>
+      <button className={css.userAuthLink} type="button" onClick={authClick}>
+        Sign in
+      </button>
       <svg width="40" height="48" className={css.icons}>
-        <use href="/symbol-defs.svg#icon-user" >
-        </use>
-      </svg>
+        <use href="/project-successful-minds-07/symbol-defs.svg#icon-Logo-2" >
+        </use></svg>
     </div>
   );
 };
 
 export default UserAuth;
-
