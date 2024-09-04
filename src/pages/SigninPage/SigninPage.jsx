045ef22ec999a13c2d702218f3141c72fc1758e3
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-09-04 09:21:16 +0300

    change App.css and Logo

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -10,7 +10,42 @@ const SigninPage = () => {
       {/* <Helmet>
         <title>SignIn to Tracker of Water</title>
       </Helmet> */}
+      {/* <picture>
+        <source
+          srcset="
+            ./img/img_about_desktop_1440@1x.png 1x,
+            ./img/img_about_desktop_1440@2x.png 2x
+          "
+          media="(min-width:1440px)"
+        />
+        <source
+          srcset="
+            ./img/img_about_desktop_1280@1x.png 1x,
+            ./img/img_about_desktop_1280@2x.png 2x
+          "
+          media="(min-width:1280px)"
+        />
+        <source
+          srcset="
+            ./img/img_about_tablet@1x.png 1x,
+            ./img/img_about_tablet@2x.png 2x
+          "
+          media="(min-width:768px)"
+        />
+        <source
+          srcset="
+            ./img/img_about_mobile@1x.png 1x,
+            ./img/img_about_mobile@2x.png 2x
+          "
+          media="(max-width:767px)"
+        />
 
+        <img
+          class="about-img"
+          src="./img/img_about_mobile@1x.png"
+          alt="Yacht at night"
+        />
+      </picture> */}
       <div className={css.container_signin}>
         <div className={css.background}>
           <h2 className={css.title}>Sign In</h2>
