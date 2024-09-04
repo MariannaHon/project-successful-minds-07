bc254a7652a8de6180fd2780cb4568ab624ff744
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2024-09-04 11:49:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-successful-minds-07/',
+  // base: '/project-successful-minds-07/',
   build: {
     sourcemap: true,
   }
