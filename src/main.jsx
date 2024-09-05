66d8cdde5cceb7dd0d8d844b68ba116d21717adf
MariannaHon <imarianna96i@gmail.com>
2024-09-05 14:46:41 +0200

    ---

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App/App.jsx';
 import 'modern-normalize';
 import './styles/globalStyles.css';
