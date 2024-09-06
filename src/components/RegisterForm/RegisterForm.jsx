cbc1b838913b66f3266bb39b5c9edda72b73b99d
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-09-06 12:45:45 +0300

    complete component styles

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -6,46 +6,45 @@ import toast from 'react-hot-toast';
 import css from './RegisterForm.module.css';
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6';
 import { useId, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const RegisterForm = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [type, setType] = useState('password');
 
   const dispatch = useDispatch();
-
   const onSubmit = (values, actions) => {
     const newUser = {
       email: values.email,
       password: values.password,
     };
-
-    if (values.password === values.repeatPassword) {
-      dispatch(register(newUser));
-      actions.resetForm();
-    } else {
-      toast.error('Passwords did not happen', {
-        position: 'top-right',
+    dispatch(register(newUser))
+      .unwrap()
+      .then(() => {})
+      .catch(() => {
+        toast.error('Passwords did not happen', {
+          position: 'top-right',
+        });
       });
-    }
+    actions.resetForm();
   };
 
   const formSchema = Yup.object().shape({
-    email: Yup.string().email().required(),
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is Required'),
     password: Yup.string()
       .min(8, 'Too short password')
       .max(64, 'Too long password')
-      .required(),
+      .required('Password is Required'),
+    repeatPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Repeat Password is Required'),
   });
 
   const togglePassInput = () => {
-    if (type === 'password') {
-      setType('text');
-      setIsOpen(false);
-    } else {
-      setType('password');
-      setIsOpen(true);
-    }
+    setType(isOpen ? 'text' : 'password');
+    setIsOpen(!isOpen);
   };
 
   const id = useId();
@@ -61,104 +60,138 @@ const RegisterForm = () => {
             onSubmit={onSubmit}
             validationSchema={formSchema}
           >
-            <Form className={css.form}>
-              <div className={css.formContainer}>
-                <label className={css.label} htmlFor="email">
-                  Enter your email
-                </label>
-                <Field
-                  className={css.input}
-                  id={`${id}-email`}
-                  type="email"
-                  name="email"
-                  autoComplete="off"
-                  placeholder="E-mail"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="span"
-                  className={css.error}
-                />
-              </div>
-              <div className={css.formContainer}>
-                <label className={css.label} htmlFor="password">
-                  Enter your password
-                </label>
-                <div className={css.iconPosition}>
-                  <Field
-                    id={`${id}-password`}
-                    className={css.input}
-                    type="password"
+            {({ errors, touched }) => (
+              <Form className={css.form} noValidate>
+                <div className={css.formContainer}>
+                  <label className={css.label} htmlFor="email">
+                    Enter your email
+                  </label>
+                  <div>
+                    <Field
+                      className={`${css.input} ${
+                        errors.email && touched.email
+                          ? errors.email === 'Email is Required'
+                            ? css.required
+                            : css.invalid
+                          : touched.email && !errors.email
+                          ? css.valid
+                          : ''
+                      }`}
+                      id={`${id}-email`}
+                      type="email"
+                      name="email"
+                      autoComplete="off"
+                      placeholder="E-mail"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="email"
+                    component="span"
+                    className={css.error}
+                  />
+                </div>
+
+                <div className={css.formContainer}>
+                  <label className={css.label} htmlFor="password">
+                    Enter your password
+                  </label>
+                  <div className={css.iconPosition}>
+                    <Field
+                      id={`${id}-password`}
+                      className={`${css.input} ${
+                        errors.password && touched.password
+                          ? errors.password === 'Too short password'
+                            ? css.short
+                            : errors.password === 'Too long password'
+                            ? css.long
+                            : errors.password === 'Password is Required'
+                            ? css.required
+                            : css.invalid
+                          : touched.password && !errors.password
+                          ? css.valid
+                          : ''
+                      }`}
+                      type={type}
+                      name="password"
+                      autoComplete="new-password"
+                      placeholder="Password"
+                    />
+
+                    {isOpen ? (
+                      <FaRegEyeSlash
+                        name="password"
+                        id={`${id}-password`}
+                        className={css.passEye}
+                        onClick={togglePassInput}
+                      />
+                    ) : (
+                      <FaRegEye
+                        name="password"
+                        id={`${id}-password`}
+                        className={css.passEye}
+                        onClick={togglePassInput}
+                      />
+                    )}
+                  </div>
+
+                  <ErrorMessage
                     name="password"
-                    autoComplete="new-password"
-                    placeholder="Password"
+                    component="span"
+                    className={css.error}
                   />
                 </div>
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className={css.error}
-                />
-                {isOpen ? (
-                  <FaRegEyeSlash
+                <div className={css.formContainer}>
+                  <label className={css.label} htmlFor="repeatPassword">
+                    Repeat password
+                  </label>
+                  <div className={css.iconPosition}>
+                    <Field
+                      id={`${id}-repeatPassword`}
+                      className={`${css.input} ${
+                        errors.repeatPassword && touched.repeatPassword
+                          ? css.mismatch
+                          : touched.repeatPassword && !errors.repeatPassword
+                          ? css.valid
+                          : ''
+                      }`}
+                      type={isOpen ? 'password' : 'text'}
+                      name="repeatPassword"
+                      autoComplete="new-password"
+                      placeholder="Repeat password"
+                    />
+
+                    {isOpen ? (
+                      <FaRegEyeSlash
+                        name="repeatPassword"
+                        id={`${id}-repeatPassword`}
+                        className={css.repPassEye}
+                        onClick={togglePassInput}
+                      />
+                    ) : (
+                      <FaRegEye
+                        name="repeatPassword"
+                        id={`${id}-repeatPassword`}
+                        className={css.repPassEye}
+                        onClick={togglePassInput}
+                      />
+                    )}
+                  </div>
+                  <ErrorMessage
                     name="repeatPassword"
-                    id={`${id}-repeatPassword`}
-                    className={css.passEye}
-                    onClick={togglePassInput}
-                  />
-                ) : (
-                  <FaRegEye
-                    name="repeatPassword"
-                    id={`${id}-repeatPassword`}
-                    className={css.passEye}
-                    onClick={togglePassInput}
-                  />
-                )}
-              </div>
-              <div className={css.formContainer}>
-                <label className={css.label} htmlFor="repeatPassword">
-                  Repeat password
-                </label>
-                <div className={css.iconPosition}>
-                  <Field
-                    id={`${id}-repeatPassword`}
-                    className={css.input}
-                    type="password"
-                    name="repeatPassword"
-                    autoComplete="new-password"
-                    placeholder="Repeat password"
+                    component="span"
+                    className={css.error}
                   />
                 </div>
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className={css.error}
-                />
-                {isOpen ? (
-                  <FaRegEyeSlash
-                    name="repeatPassword"
-                    id={`${id}-repeatPassword`}
-                    className={css.repPassEye}
-                    onClick={togglePassInput}
-                  />
-                ) : (
-                  <FaRegEye
-                    name="repeatPassword"
-                    id={`${id}-repeatPassword`}
-                    className={css.repPassEye}
-                    onClick={togglePassInput}
-                  />
-                )}
-              </div>
-              <button className={css.button} type="submit">
-                Sign Up
-              </button>
-            </Form>
+                <button className={css.button} type="submit">
+                  Sign Up
+                </button>
+              </Form>
+            )}
           </Formik>
           <div className={css.image} alt="BottleLogo">
-            <NavLink className={css.link} to="/signin">
+            <Link className={css.link} to="/signin">
               Sign in
-            </NavLink>
+            </Link>
           </div>
         </div>
       </div>
