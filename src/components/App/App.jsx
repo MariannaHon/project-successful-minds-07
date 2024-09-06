96183bfc3a64d34b1417a7d7d103f7d4ae13dec7
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-09-06 20:35:25 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -76,7 +76,7 @@ export default function App() {
               }
             />
             <Route
-              path="/home"
+              path="/password"
               element={
                 <PrivateRoute
                   component={UpdatePasswordPage}
