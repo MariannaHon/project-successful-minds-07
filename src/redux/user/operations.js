fb3e7ff5e0424592b049c00bec25cac0f46b48d4
Yuliia Diachenko <zirkanew82@gmail.com>
2024-09-08 13:42:53 +0300

    fix auth error

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// const setAuthHeader = (token) => {
-//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 axios.defaults.baseURL = 'https://successful-minds-db.onrender.com/';
 
@@ -12,7 +12,7 @@ export const fetchUser = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await axios.get('get/user');
-      // setAuthHeader(response.data.accessToken);
+      setAuthHeader(response.data.accessToken);
       return response.data;        
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +31,7 @@ export const updateUser = createAsyncThunk(
         email,
         password,
       });
-      // setAuthHeader(response.data.accessToken);
+      setAuthHeader(response.data.accessToken);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -44,7 +44,7 @@ export const changeAvatar = createAsyncThunk(
   async ({avatarUrl},thunkAPI) => {
     try {
     const response = await axios.patch('update/avatar', {avatarUrl});
-    // setAuthHeader(response.data.accessToken);
+    setAuthHeader(response.data.accessToken);
     return response.data;
   } catch (error) {
     console.log(error)
