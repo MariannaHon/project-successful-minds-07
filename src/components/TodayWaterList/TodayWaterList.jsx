72d80ec8a2593a558e21e289c2bcb15049550527
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-09-08 19:33:37 +0300

    add function commit

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -50,3 +50,47 @@ const TodayWaterList = () => {
 };
 
 export default TodayWaterList;
+
+
+
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchWaterPerDay } from '../../redux/operations';
+// import { selectWaterPerDayArr } from '../../redux/selectors';
+// import { HiOutlinePencilSquare, HiOutlineTrash } from "react-icons/hi2";
+// import { CiGlass } from "react-icons/ci";
+// import css from './TodayWaterList.module.css';
+
+// const TodayWaterList = () => {
+//   const dispatch = useDispatch();
+//   const waterEntries = useSelector(selectWaterPerDayArr); 
+
+//     useEffect(() => {
+//     const todayDate = new Date().toLocaleDateString(); 
+//     dispatch(fetchWaterPerDay(todayDate)); 
+//   }, [dispatch]);
+
+//     return (
+//     <div className={css.todayWaterList}>
+//       <h2 className={css.title}>Today</h2>
+//       <ul className={css.list}>
+//         {waterEntries && waterEntries.map(entry => (
+//           <li key={entry.id} className={css.item}>
+//             <div className={css.value}>
+//               <CiGlass className={css.iconGlass} />
+//               <p className={css.amount}>{entry.amount} ml</p>
+//               <p className={css.time}>{entry.time}</p>
+//             </div>
+//             <div className={css.btnAll}>
+//               <button className={css.btn}><HiOutlinePencilSquare className={css.iconPencil}/></button>
+//               <button className={css.btn}><HiOutlineTrash className={css.iconDelete}/></button>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//       <button className={css.addWaterButton}>+ Add water</button>
+//     </div>
+//   );
+// };
+
+// export default TodayWaterList;
--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -48,3 +48,81 @@ const WaterRatioPanel = ({ progress, handleAddWater }) => {
 
 export default WaterRatioPanel;
 
+
+// import { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// // import TodayListModal from "../TodayListModal/TodayListModal.jsx"
+// import { CgAdd } from "react-icons/cg";
+// import { fetchWaterPerDay } from "../../redux/water/operations.js";
+// import { selectWaterPerDay, selectWaterNorma } from "../../redux/water/selectors.js";
+// import css from './WaterRatioPanel.module.css';
+
+// const WaterRatioPanel = () => {
+//   const dispatch = useDispatch();
+//   const dailyWaterRecords = useSelector(selectWaterPerDay);
+//   const waterNorm = useSelector(selectWaterNorma);
+
+//   const [consumedWaterPercentage, setConsumedWaterPercentage] = useState(0);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   // Отримання даних про воду за сьогоднішню дату
+//   useEffect(() => {
+//     const todayDate = new Date().toLocaleDateString();
+//     dispatch(fetchWaterPerDay(todayDate));
+//   }, [dispatch]);
+
+//   // Обчислення відсотка спожитої води
+//   useEffect(() => {
+//     if (dailyWaterRecords && waterNorm) {
+//       const totalWaterConsumed = dailyWaterRecords.reduce((total, record) => total + record.amount, 0);
+//       const percentageConsumed = Math.min((totalWaterConsumed / waterNorm) * 100, 100);
+//       setConsumedWaterPercentage(percentageConsumed);
+//     }
+//   }, [dailyWaterRecords, waterNorm]);
+
+//   // Обробка відкриття та закриття модального вікна
+//   const handleAddWater = () => {
+//     setIsModalOpen(true); // Відкриваємо модальне вікно
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false); // Закриваємо модальне вікно
+//   };
+
+//   return (
+//     <div className={css.container}>
+//       <h2 className={css.title}>Today</h2>
+
+//       <div className={css.progressBarContainer}>
+//         <div className={css.progressBar}>
+//           <div className={css.progress} style={{ width: `${consumedWaterPercentage}%` }} />
+//           <div className={css.thumb} />
+//           <div className={css.progressTextNumber}>
+//             <span className={css.progressText}>0%</span>
+//             <span className={css.progressTextM}>50%</span>
+//             <span className={css.progressText}>100%</span>
+//           </div>
+//           <div className={css.rangeWaterValue}>
+//             {consumedWaterPercentage}%
+//           </div>
+//         </div>
+//         <button className={css.addWaterButton} onClick={handleAddWater}>
+//           <CgAdd className={css.icon}/> Add Water
+//         </button>
+//       </div>
+
+//       {/* Заготовка під модальне вікно */}
+//       {isModalOpen && (
+//         <div className={css.modalOverlay}>
+//           <div className={css.modalContent}>
+//             <h3>Add Water Entry</h3>
+//             <button onClick={closeModal}>Close</button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default WaterRatioPanel;
+
