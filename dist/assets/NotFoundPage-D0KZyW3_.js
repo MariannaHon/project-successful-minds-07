e57855ec2159c11e905e4b6f7a9bd3c7c16b4b43
MariannaHon <imarianna96i@gmail.com>
2024-09-09 03:49:51 +0200

    ---

---
+++ dist/assets/NotFoundPage-D0KZyW3_.js
@@ -0,0 +1,2 @@
+import{j as e,L as t}from"./index-B0LgItY_.js";import s from"./HomePage-DMlfv7b3.js";const n="_text_19zlo_1",l="_link_19zlo_23",o={text:n,link:l};function c(){return e.jsxs("p",{className:o.text,children:["Sorry, page not found! Please go to",e.jsx(t,{to:"/welcome",className:o.link,children:"Welcome Page!"}),e.jsx(s,{})]})}export{c as default};
+//# sourceMappingURL=NotFoundPage-D0KZyW3_.js.map
