b2dcae19fa747ee77025ac18ef6d4834f182e49b
MariannaHon <imarianna96i@gmail.com>
2024-09-11 16:07:13 +0200

    fix add and date

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -11,8 +11,6 @@ export const fetchWaterPerDay = createAsyncThunk(
     try {
       const response = await axios.get(`/water/today`);
 
-      console.log(response.data.data);
-
       return response.data.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
