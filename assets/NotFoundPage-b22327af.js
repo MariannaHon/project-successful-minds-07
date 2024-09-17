c051eee46b5c32ef2ac99b478e673bd431a6d3aa
MariannaHon <148065963+MariannaHon@users.noreply.github.com>
2024-09-17 20:55:53 +0000

    Deploying to gh-pages from @ MariannaHon/project-successful-minds-07@92ababc06c074b56277e3fa4979c9b4508dc3862 🚀

--- assets/NotFoundPage-b22327af.js
+++
@@ -1,2 +0,0 @@
-import{j as t,W as o}from"./index-832a9f01.js";const s="_text_vgwlj_1",n="_link_vgwlj_12",e={text:s,link:n};function a(){return t.jsxs("p",{className:e.text,children:["Sorry, page not found! Please go to",t.jsx(o,{to:"/welcome",className:e.link,children:"Welcome Page!"})]})}export{a as default};
-//# sourceMappingURL=NotFoundPage-b22327af.js.map
