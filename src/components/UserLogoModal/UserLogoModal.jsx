184fd1be53295ca9ab991eb86276fc7e43950d99
MariannaHon <imarianna96i@gmail.com>
2024-09-18 13:37:11 +0200

    fix modal
    fixed positioning of setting modal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,46 +1,11 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import css from './UserLogoModal.module.css';
 import { GoChevronDown } from 'react-icons/go';
 import SettingModal from '../SettingModal/SettingModal.jsx';
 import UserLogoutModal from '../UserLogoutModal/UserLogoutModal.jsx';
-import { styled } from '@mui/material/styles';
 
-const CustomBox = styled(Box)(({ theme }) => ({
-  padding: 'var(--icon-size-smaller)',
-  position: 'fixed',
-  borderRadius: 'var(--border-radius-smaller)',
-  backgroundColor: 'var(--primary-white)',
-  boxShadow: 'var(--box-shadow-light)',
-  border: 'none',
-  width: '118px',
-  height: '88px',
-  top: 100,
-  right: 0,
-  transform: 'translate(-50%, -50%)',
-  zIndex: 1300,
-  [theme.breakpoints.down('xs')]: {
-    top: 120,
-    right: 0,
-  },
-  [theme.breakpoints.between('sm', 'md')]: {
-    top: 120,
-    right: 0,
-  },
-  [theme.breakpoints.between('md', 'lg')]: {
-    top: 120,
-    right: 100,
-  },
-  [theme.breakpoints.between('lg', 'xl')]: {
-    top: 120,
-    right: 100,
-  },
-  [theme.breakpoints.up('xl')]: {
-    top: 120,
-    right: 330,
-  },
-}));
+
 
 const UserLogoModal = () => {
   const [open, setOpen] = React.useState(false);
@@ -62,12 +27,10 @@ const UserLogoModal = () => {
             },
           }}
         >
-          <CustomBox>
-            <div className={css.container}>
-              <SettingModal />
-              <UserLogoutModal />
-            </div>
-          </CustomBox>
+          <div className={css.container}>
+            <SettingModal />
+            <UserLogoutModal />
+          </div>
         </Modal>
       </div>
     </div>
